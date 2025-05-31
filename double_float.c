6a19c1847c4afd5dfb556c54da597f6898068f14
kulekule2003 <okureenock@gmail.com>
2025-05-31 12:01:41 +0300

    do, double and float and if-else

---
+++ double_float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    // creating float and double variables
+
+    float f = 0.3;
+    double d = 10.67;
+
+    printf("Float value: %f\n", f);
+    printf("double value: %f\n", d);
+
+    return 0;
+}
---
+++ if_else.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+
+    int a = 11;
+
+    // if condition and block
+
+    if (a<11) {
+        printf("A is less than 11");
+    } 
+    else {
+        printf("A iis equal to or "
+        "greater than 11");
+    }
+    return 0;
+}
