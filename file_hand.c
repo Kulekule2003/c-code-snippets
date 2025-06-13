18a4c4b06881d22df9cbff623b278c2b5270bfe4
kulekule2003 <okureenock@gmail.com>
2025-06-13 18:42:44 +0300

    added more ccode

--- file_hand.c
+++ file_hand.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
-    
+
+    // file pointer to store the 
+    // value return by fopen
+
+    FILE* fptr;
+
+    // opening the file in read mode
+    fptr = fopen("okure.txt", "w");
+
+    // checking if the file is 
+    // open successfully
+
+    if (fptr == NULL) {
+        printf("the file is not opened");
+    } else {
+        printf("the file is created");
+    }
     return 0;
 }
