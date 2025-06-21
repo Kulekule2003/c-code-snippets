19edef40cfe7f1aacf6c164a97096ae10ee793d5
kulekule2003 <okureenock@gmail.com>
2025-06-21 23:19:17 +0300

    more code

--- file_hand.c
+++ file_hand.c
@@ -5,18 +5,20 @@
 int main() {
     FILE* fptr;
 
-    char data[50];
-    fptr = fopen("okure.txt", "r");
+    char data[50] = "GeeksforGeeks-A Computer "
+                    "Science Portal for Geeks";
+
+    fptr = fopen("ok.txt", "w");
 
     if (fptr == NULL) {
         printf("file.txt file failed to open");
     } else {
         printf("The file is now opened.\n");
+        fputs(data, fptr);
+        fputs("\n", fptr);
+        fseek(fptr, -6, SEEK_END);
 
-        while (fgets(data, 50, fptr) != NULL) {
-            printf("%s", data);
-        }
-
+        fputs("okurenock", fptr);
         fclose(fptr);
     }
     return 0;
